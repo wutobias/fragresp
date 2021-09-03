8e353a1c9d7dd6d5a32bd3020966e5aa533541ce
Tobias Wulsdorf <tobias.wulsdorf@uni-marburg.de>
2021-09-03 16:19:45 -0700

    Better handling of aux programs

--- fragresp/aux_progs.py
+++ fragresp/aux_progs.py
@@ -1,10 +1,33 @@
-import os
 
-AMBERHOME=os.getenv('AMBERHOME')
-if AMBERHOME == None:
-    raise Warning("Environment variable AMBERHOME not set!")
 
-ante_exe    = AMBERHOME+"/bin/antechamber"
-respgen_exe = AMBERHOME+"/bin/respgen"
-espgen_exe  = AMBERHOME+"/bin/espgen"
-resp_exe    = AMBERHOME+"/bin/resp"
+### see https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+ante_exe    = which("antechamber")
+respgen_exe = which("respgen")
+espgen_exe  = which("espgen")
+resp_exe    = which("resp")
+
+if ante_exe == None:
+    raise Warning("antechamber not found")
+if respgen_exe == None:
+    raise Warning("respgen not found")
+if espgen_exe == None:
+    raise Warning("espgen not found")
+if resp_exe == None:
+    raise Warning("resp not found")
